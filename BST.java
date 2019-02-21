93c4f9027422152a79370409a2e85f590f851d52
billlhw <47807346+billlhw@users.noreply.github.com>
2019-02-20 20:32:16 -0700

    Update BST.java

--- BST.java
+++ BST.java
@@ -1,58 +1,64 @@
 package Game;
+
 /*
  * Sorts data of user's times taken for each move in a binary search tree
  */
 public class BST {
-   BstNode rootNode;
+	BstNode rootNode;
 
+	// constructor: Root node added at the time of creation of
+	// the tree
 	public BST(int i) // constructor: Root node added at the time of creation of
 	// the tree
 	{
 		rootNode = new BstNode(i);
 	}
 
-		public void addNode(int i) {
-			BstNode currentNode = rootNode;
-			boolean finished = false;
-			do {
-				BstNode curLeftNode = currentNode.leftNode;
-				BstNode curRightNode = currentNode.rightNode;
-				int curIntData = currentNode.intData;
+	public void addNode(int i) {
+		BstNode currentNode = rootNode;
+		boolean finished = false;
+		do {
+			BstNode curLeftNode = currentNode.leftNode;
+			BstNode curRightNode = currentNode.rightNode;
+			int curIntData = currentNode.intData;
 
-				if (i > curIntData) // look down the right branch
+			if (i > curIntData) // look down the right branch
+			{
+				if (curRightNode == null) { // create a new node referenced with
+											// currentNode.rightNode
+					currentNode.rightNode = new BstNode(i);
+					finished = true;
+				} else // keep looking by assigning a new current node one level
+						// down
 				{
-					if (curRightNode == null) { // create a new node referenced with
-												// currentNode.rightNode
-						currentNode.rightNode = new BstNode(i);
-						finished = true;
-					} else // keep looking by assigning a new current node one level
-							// down
-					{
-						currentNode = currentNode.rightNode;
-					}
-				} else // look down the left branch
-				{
-					if (curLeftNode == null) { // create a new node referenced with
-												// currentNode.leftNode
-						currentNode.leftNode = new BstNode(i);
-						finished = true;
-					} else { // keep looking by assigning a new current node one
-								// level down
-
-						currentNode = currentNode.leftNode;
-					}
+					currentNode = currentNode.rightNode;
 				}
-			} while (!finished);
-		}
-		BstNode ParentNode;
-		public void printSideways() {
-			printSideways(rootNode, "");
+			} else // look down the left branch
+			{
+				if (curLeftNode == null) { // create a new node referenced with
+											// currentNode.leftNode
+					currentNode.leftNode = new BstNode(i);
+					finished = true;
+				} else { // keep looking by assigning a new current node one
+							// level down
+
+					currentNode = currentNode.leftNode;
+				}
 			}
-		private void printSideways(BstNode root,String indent) {
-			if (root != null) {
-				printSideways(root.rightNode, indent + "  ");
-				System.out.println(indent + root.intData);
-				printSideways(root.leftNode, indent + "  ");
-			}//Recursive methods to print the binary tree.
-			}
+		} while (!finished);
+	}
+
+	BstNode ParentNode;
+
+	public void printSideways() {
+		printSideways(rootNode, "");
+	}
+
+	private void printSideways(BstNode root, String indent) {
+		if (root != null) {
+			printSideways(root.rightNode, indent + "  ");
+			System.out.println(indent + root.intData);
+			printSideways(root.leftNode, indent + "  ");
+		} // Recursive methods to print the binary tree.
+	}
 }
